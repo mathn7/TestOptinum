d650ac29e7b1909169e72a18abc585c02f1b9271
MohamedElWaghf <hindahwakhfe@gmail.com>
2020-06-15 16:42:52 +0200

    Création du module

---
+++ src/Test_Optinum.jl
@@ -0,0 +1,8 @@
+module Test_Optinum
+
+include("test_Algo_Newton.jl")
+include("test_Lagrangien_Augmente.jl")
+include("test_pas_de_cauchy.jl")
+include("test_regions_de_confiance.jl")
+
+end # module
