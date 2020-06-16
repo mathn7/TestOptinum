b47a681f47f2a586b218ba5bfb8ad2dd90423865
nsaloua <58554997+nsaloua@users.noreply.github.com>
2020-06-16 16:21:15 +0200

    Update test_regions_de_confiance.jl

--- src/test_regions_de_confiance.jl
+++ src/test_regions_de_confiance.jl
@@ -34,7 +34,7 @@ function test_regions_de_confiance(afficher,Regions_De_Confiance::Function)
 	maxits = 5000
 	delta0_1 = 2
 	delta0_2 = 1
-	eps = 0
+	eps = 1e-8
 
 	option1 =[deltaMax,gamma1,gamma2,eta1,eta2,delta0_1,maxits,tol]
 	option2 =[deltaMax,gamma1,gamma2,eta1,eta2,delta0_2,maxits,tol]
@@ -43,7 +43,7 @@ function test_regions_de_confiance(afficher,Regions_De_Confiance::Function)
 	normerreur = 1e-2
 
 	"#test avec la fonction f1 et point initial x011"
-	RC_x_min11, RC_fmin11, RC_flag11, RC_nb_iters11 = Regions_De_Confiance("cauchy",fct1,grad_fct1,hess_fct1,x011,option1,eps)
+	RC_x_min11, RC_fmin11, RC_flag11, RC_nb_iters11 = Regions_De_Confiance("cauchy",fct1,grad_fct1,hess_fct1,x011,option1)
 	#affichage des résultats du test
 	if (afficher)
 		afficher_resultats("cauchy","fonction 1","x011",RC_x_min11,RC_fmin11, RC_flag11,sol_exacte_fct1,RC_nb_iters11)
@@ -51,7 +51,7 @@ function test_regions_de_confiance(afficher,Regions_De_Confiance::Function)
 
 
 	"#test avec la fonction f1 et point initial x012"
-	RC_x_min12, RC_fmin12, RC_flag12, RC_nb_iters12  = Regions_De_Confiance("cauchy",fct1,grad_fct1,hess_fct1,x012,option1,eps)
+	RC_x_min12, RC_fmin12, RC_flag12, RC_nb_iters12  = Regions_De_Confiance("cauchy",fct1,grad_fct1,hess_fct1,x012,option1)
 	#affichage des résultats du test
 	if (afficher)
 		afficher_resultats("cauchy","fonction 1","x012",RC_x_min12,RC_fmin12, RC_flag11,sol_exacte_fct1,RC_nb_iters12)
@@ -60,7 +60,7 @@ function test_regions_de_confiance(afficher,Regions_De_Confiance::Function)
 
 
 	"#test avec la fonction f2 et point initial x021"
-	RC_x_min21, RC_fmin21, RC_flag21, RC_nb_iters21,  = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x021,option2,eps)
+	RC_x_min21, RC_fmin21, RC_flag21, RC_nb_iters21,  = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x021,option2)
 	#affichage des résultats du test
 	if (afficher)
 		afficher_resultats("cauchy","fonction 2","x021",RC_x_min21,RC_fmin21, RC_flag21,sol_exacte_fct2,RC_nb_iters21)
@@ -69,49 +69,49 @@ function test_regions_de_confiance(afficher,Regions_De_Confiance::Function)
 
 
 	"#test avec la fonction f2 et point initial x022"
-	RC_x_min22, RC_fmin22, RC_flag22, RC_nb_iters22 = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x022,option2,eps)
+	RC_x_min22, RC_fmin22, RC_flag22, RC_nb_iters22 = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x022,option2)
 	if (afficher)
 		afficher_resultats("cauchy","fonction 2","x022",RC_x_min22,RC_fmin22, RC_flag22,sol_exacte_fct2,RC_nb_iters22)
 	end
 
 
 	"#test avec la fonction f2 et point initial x023"
-	RC_x_min23, RC_fmin23, RC_flag23, RC_nb_iters23 = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x023,option2,eps)
+	RC_x_min23, RC_fmin23, RC_flag23, RC_nb_iters23 = Regions_De_Confiance("cauchy",fct2,grad_fct2,hess_fct2,x023,option2)
 	if (afficher)
 		afficher_resultats("cauchy","fonction 2","x023",RC_x_min23,RC_fmin23, RC_flag23,sol_exacte_fct2,RC_nb_iters23)
 	end
 
 
 	"#test avec la fonction f1 et point initial x011"
-	gct_x_min11, RC_fmin11, RC_flag11, RC_nb_iters11= Regions_De_Confiance("gct",fct1,grad_fct1,hess_fct1,x011,option1,eps)
+	gct_x_min11, RC_fmin11, RC_flag11, RC_nb_iters11= Regions_De_Confiance("gct",fct1,grad_fct1,hess_fct1,x011,option1)
 	if (afficher)
 		afficher_resultats("gct","fonction 1","x011",RC_x_min11,RC_fmin11, RC_flag11,sol_exacte_fct1,RC_nb_iters11)
 	end
 
 
 	"#test avec la fonction f1 et point initial x012"
-	gct_x_min12, RC_fmin12, RC_flag12, RC_nb_iters12 = Regions_De_Confiance("gct",fct1,grad_fct1,hess_fct1,x012,option1,eps)
+	gct_x_min12, RC_fmin12, RC_flag12, RC_nb_iters12 = Regions_De_Confiance("gct",fct1,grad_fct1,hess_fct1,x012,option1)
 	if (afficher)
 		afficher_resultats("gct","fonction 1","x012",RC_x_min12,RC_fmin12, RC_flag12,sol_exacte_fct1,RC_nb_iters12)
 	end
 
 
 	"#test avec la fonction f2 et point initial x021"
-	gct_x_min21, RC_fmin21, RC_flag21, RC_nb_iters21 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x021,option2,eps)
+	gct_x_min21, RC_fmin21, RC_flag21, RC_nb_iters21 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x021,option2)
 	if (afficher)
 		afficher_resultats("gct","fonction 2","x021",RC_x_min21,RC_fmin21, RC_flag21,sol_exacte_fct2,RC_nb_iters21)
 	end
 
 
 	"#test avec la fonction f2 et point initial x022"
-	gct_x_min22, RC_fmin22, RC_flag22, RC_nb_iters22 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x022,option2,eps)
+	gct_x_min22, RC_fmin22, RC_flag22, RC_nb_iters22 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x022,option2)
 	if (afficher)
 		afficher_resultats("gct","fonction 2","x022",RC_x_min22,RC_fmin22, RC_flag22,sol_exacte_fct2,RC_nb_iters22)
 	end
 
 
 	"#test avec la fonction f2 et point initial x023"
-	gct_x_min23, RC_fmin23, RC_flag23, RC_nb_iters23 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x023,option2,eps)
+	gct_x_min23, RC_fmin23, RC_flag23, RC_nb_iters23 = Regions_De_Confiance("gct",fct2,grad_fct2,hess_fct2,x023,option2)
 	if (afficher)
 		afficher_resultats("gct","fonction 2","x023",RC_x_min11,RC_fmin23, RC_flag23,sol_exacte_fct2,RC_nb_iters23)
 	end
