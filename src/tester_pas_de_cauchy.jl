acf02127db0384ae2c745ba70ef42bec64d6e83f
relmonta <46019567+relmonta@users.noreply.github.com>
2020-06-17 13:56:50 +0200

    Update tester_pas_de_cauchy.jl

--- src/tester_pas_de_cauchy.jl
+++ src/tester_pas_de_cauchy.jl
@@ -35,8 +35,8 @@ function tester_pas_de_cauchy(affichage,Pas_De_Cauchy::Function)
 
 	res = @testset "Pas de Cauchy" begin 
 		@test (e1 == 0) && (isapprox(s1,sol_exacte_q1,atol=tol_erreur))
-		@test (e2 == 0) && (isapprox(s2,sol_exacte_q2,atol=tol_erreur))
-		@test (e3 == 0) && (isapprox(s3,sol_exacte_q3,atol=tol_erreur))		  
+		@test (e2 == 1) && (isapprox(s2,sol_exacte_q2,atol=tol_erreur))
+		@test (e3 == 1) && (isapprox(s3,sol_exacte_q3,atol=tol_erreur))		  
 	end
 	println("\n")
 
