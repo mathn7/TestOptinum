834e620a9469800ca766e7d59e4228ab85a24464
relmonta <46019567+relmonta@users.noreply.github.com>
2020-06-17 09:30:33 +0200

    Update Test_Optinum.jl

--- src/Test_Optinum.jl
+++ src/Test_Optinum.jl
@@ -6,7 +6,7 @@ export pts1,pts2
 export sol_exacte_fct1,fct1,grad_fct1,hess_fct1
 export sol_exacte_fct2,fct2,grad_fct2,hess_fct2
 export sol_fct1_augm,contrainte1,grad_contrainte1,hess_contrainte1
-export sol_fct2_augm,contrainte2,grad_contrainte2,hess_contrainte2,phi
+export sol_fct2_augm,contrainte2,grad_contrainte2,hess_contrainte2
 
 include("test_Algo_Newton.jl")
 export test_Algo_Newton
