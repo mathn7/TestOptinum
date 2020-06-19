69bfc692009c5c5a3e413c41ffacf79401c0bf47
relmonta <46019567+relmonta@users.noreply.github.com>
2020-06-19 19:37:05 +0200

    Update and rename effacer_stacktrace.jl to stacktrace.jl

--- src/stacktrace.jl
+++ src/stacktrace.jl
@@ -1,17 +1,16 @@
 using Test
 
-function effacer_stacktrace()
+function stacktrace()
     Test.eval(quote
        function record(ts::DefaultTestSet, t::Union{Fail, Error})
            if myid() == 1
-               printstyled(ts.description, ": ", color=:white)
-               # don't print for interrupted tests
+               #printstyled(ts.description, ": ", color=:white)
+               # ne pas afficher pour les tests interrompus
                if !(t isa Error) || t.test_type != :test_interrupted
                    # print(t) # afficher le resultat et la solution attendu                    
                    if !isa(t, Error)
                        # Base.show_backtrace(stdout, scrub_backtrace(backtrace())) # afficher le backtrace
-                   end
-                   println()
+                   end                   
                end
            end
            push!(ts.results, t)
