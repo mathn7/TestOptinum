a850073d9a416dab5eb1405b061f503ef5924505
relmo <elmontassir.rachid1937@gmail.com>
2020-06-22 12:27:26 +0200

    update cacher_stacktrace

--- src/cacher_stacktrace.jl
+++ src/cacher_stacktrace.jl
@@ -1,23 +1,24 @@
 using Test
 @doc doc"""
 
-Cacher les stacktraces des tests, pour les remettre, décommentez les lignes 11 et 16.
+Cacher les traces d'appels des tests erronés ou échoués, pour les remettre, décommentez les lignes 11 et 16.
 
 """
 function cacher_stacktrace()
     Test.eval(quote
        function record(ts::DefaultTestSet, t::Union{Fail, Error})
-           if myid() == 1
-               #printstyled(ts.description, ": ", color=:white)
+           if myid() == 1               
+               #printstyled(ts.description, ": ", color=:white) # afficher la description du testset
                # ne pas afficher pour les tests interrompus
                if !(t isa Error) || t.test_type != :test_interrupted
                    # print(t) # afficher le resultat et la solution attendu                    
                    if !isa(t, Error)
-                       # Base.show_backtrace(stdout, scrub_backtrace(backtrace())) # afficher le backtrace
+                       # Base.show_backtrace(stdout, scrub_backtrace(backtrace())) # afficher la trace d'appels
                    end                   
                end
            end
            push!(ts.results, t)
+           print(ts.results)
            t, isa(t, Error) || backtrace()
        end
        end)
